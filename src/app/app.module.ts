bed715b09929587c311a6dacc7c6d645065da88a
patrycja.czerwinska <patrycja.czerwinska@synaptikdigital.com>
2018-07-02 16:23:02 +0100

    add angular ionic zip package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { IonicStorageModule, Storage } from '@ionic/storage';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import {IonicUnzip} from "angular-ionic-unzip";
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { Items } from '../mocks/providers/items';
@@ -66,7 +67,8 @@ export function provideSettings(storage: Storage) {
     { provide: Settings, useFactory: provideSettings, deps: [Storage] },
     // Keep this to enable Ionic's runtime error handling during development
     { provide: ErrorHandler, useClass: IonicErrorHandler },
-    DataProvider
+    DataProvider,
+    IonicUnzip
   ]
 })
 export class AppModule { }
